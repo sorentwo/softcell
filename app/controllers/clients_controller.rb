99de27f3160d88c5dff8f6b0353b298363772039
Parker Selbert <parker@sorentwo.com>
2011-04-27 10:38:22 -0500

    Ability to create clients, show invoice

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,4 +2,8 @@ class ClientsController < ApplicationController
   inherit_resources
 
   respond_to :html
+
+  def create
+    create! { dashboard_url }
+  end
 end
--- spec/controllers/clients_controller_spec.rb
+++ spec/controllers/clients_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe ClientsController do
-  # Leveraging inherited resources
+  before do
+    session[:persisted] = true
+  end
+
+  describe 'on POST to create' do
+    let(:client) { mock_model(Client) }
+    before do
+      Client.should_receive(:new).and_return(client)
+      client.should_receive(:save).and_return(true)
+
+      post :create, client: {}
+    end
+
+    it { should respond_with(:redirect) }
+    it { should redirect_to(dashboard_url) }
+  end
 end
