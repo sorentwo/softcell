1a0a64fb1d21792f0cd5269fbdbcd3b57986cdb1
Parker Selbert <parker@sorentwo.com>
2011-05-02 22:53:11 -0500

    Reflect previous item cost when editing an invoice

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,12 +5,14 @@
       $line_items          = $('input.item-cost', $line_item_container),
       $line_item_total     = $('#line-item-total', $line_item_container);
 
-  $line_items.bind('keyup', function() {
+  $line_items.bind('change keyup', update_total)
+
+  function update_total() {
     var values = _.map($line_items, function(input) { return Number(input.value) }),
         total  = _.inject(values, function(sum, num) { return sum + num }, 0);
 
     $line_item_total.val('$' + total)
-  })
+  }
 
   // Inline Additions
 })(jQuery);
