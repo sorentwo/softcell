14be8bdc8c693042300080cdc0afb8526fe2b5ae
Parker Selbert <parker@sorentwo.com>
2015-07-21 07:02:08 -0500

    Use multiple var declarations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,4 @@
 (function($) {
-
-  // Invoice Totalling
   var $line_item_container = $('#line-items'),
       $line_items          = $('input.item-cost', $line_item_container),
       $line_item_total     = $('#line-item-total', $line_item_container);
@@ -8,11 +6,9 @@
   $line_items.bind('change keyup', update_total)
 
   function update_total() {
-    var values = _.map($line_items, function(input) { return Number(input.value) }),
-        total  = _.inject(values, function(sum, num) { return sum + num }, 0);
+    var values = _.map($line_items, function(input) { return Number(input.value) });
+    var total  = _.inject(values, function(sum, num) { return sum + num }, 0);
 
     $line_item_total.val('$' + total)
   }
-
-  // Inline Additions
 })(jQuery);
